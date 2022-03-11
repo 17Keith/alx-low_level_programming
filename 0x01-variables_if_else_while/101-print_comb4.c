8ce19f316da577a247269f65d2ff8aa11395cfab
17Keith <kayomega7@gmail.com>
2022-03-11 15:11:11 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+        int tens;
+        int ones;
+	int hundreds;
+
+        for (tens = 0; tens <= 9; tens++)
+		for (ones = tens + 1; ones <= 9; ones++)
+
+        {
+                for (hundreds =ones + tens + 1; hundreds <= 9; hundreds++)
+                {
+                        putchar(tens + '0');
+                        putchar(ones + '0');
+			putchar(ones + '0');
+                        if (hundreds < 10)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
+}
