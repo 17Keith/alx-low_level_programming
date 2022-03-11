c897dd8773911acfa2188b8a943b2f931d9ceb96
17Keith <kayomega7@gmail.com>
2022-03-11 15:56:32 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
 /**
-* main - Print combinations of two digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints combination of three digit numbers without repeating 0 1 and 2
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
-        int tens;
-        int ones;
-	int hundreds;
+	int tens, ones, hundreds;
 
-        for (tens = 0; tens <= 9; tens++)
-		for (ones = tens + 1; ones <= 9; ones++)
+	for (tens = '0'; tens < '9'; tens++)
+	{
+		for (ones = tens + 1; ones <= '9'; ones++)
+		{
+			for (hundreds = ones + 1; hundreds <= '9'; hundreds++)
+			{
+				if ((ones != tens) != hundreds)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(hundreds);
 
-        {
-                for (hundreds =ones + tens + 1; hundreds <= 9; hundreds++)
-                {
-                        putchar(tens + '0');
-                        putchar(ones + '0');
-			putchar(ones + '0');
-                        if (hundreds < 10)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+					if (tens == '7' && ones == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
