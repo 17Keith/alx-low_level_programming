db381ab70d9d03bb6afedaef82b9cfe00d84a379
17Keith <kayomega7@gmail.com>
2022-03-11 11:40:03 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+
+ * Prints out all numbers from base 10
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+
+{
+
+    int digit;
+
+    for (digit = 0; digit < 10; digit++)
+
+    printf("%i",digit);
+
+    putchar('\n');
+
+    return (0);
+
+}
