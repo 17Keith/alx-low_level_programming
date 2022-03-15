80754d81ec7606242a7d57e483891373e047fb26
17Keith <kayomega7@gmail.com>
2022-03-15 06:57:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+    int i;
+    char P[] = "abcdefghijklmnopqrstuvwxyz";
+    for(i = 0; i < 8; i++)
+    {
+    _putchar(P[i]);
+    }
+    _putchar('\n');
+}
