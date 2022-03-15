efaefc58f89b00ff47ef500e4de609e942bff372
17Keith <kayomega7@gmail.com>
2022-03-15 05:06:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,12 @@ int main(void)
 	int i;
 	char P[] = "_putchar";
 
-		for (i = 0; i < 9; i++);
+		for (i = 0; i < 9; i++)
+	
 		{
-			putchar(p[i]);
+			putchar(P[i]);
 		}
+	
 	putchar('\n');
 	return (0);
 }
