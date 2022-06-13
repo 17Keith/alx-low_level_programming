36e562848f972c5c116424592985b8d015e5f93b
17Keith <kayomega7@gmail.com>
2022-06-14 00:22:27 +0300

    Dynamic

---
+++ 0x18-dynamic_libraries/lib1/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strspn - Gets the length of a prefix substring.
+ *
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
